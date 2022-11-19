527d7694f72cff081b31ac865c24524fdb09b4d0
Qiushi Yan <qiushi.yann@gmail.com>
2022-11-19 00:17:14 -0600

    add calendar plot

---
+++ scripts/calendar.R
@@ -0,0 +1,192 @@
+library(tidyverse)
+library(rvest)
+library(lubridate)
+library(ggthemes)
+library(ggiraph)
+library(pkgsearch)
+library(furrr)
+library(httr)
+
+plan(multisession)
+
+
+github_data <- function(from = NULL, user = "qiushiyan") {
+  if (is.null(from)) {
+    url <- str_glue("https://github.com/users/{user}/contributions")
+  } else {
+    url <- str_glue("https://github.com/users/{user}/contributions?from={from}")
+  }
+
+  html <- xml2::read_html(url)
+
+  calendar <- html_node(html, ".js-calendar-graph-svg")
+  calendar_days <- html_nodes(calendar, "rect")
+  gh_data <- tibble(
+    date = as.Date(html_attr(calendar_days, "data-date")),
+    count = html_attr(calendar_days, "data-count"),
+    type = "github",
+    tip = str_glue("{count} contributions"),
+    link = str_glue("https://github.com/{user}?from={date}&to={date}&tab=overview"),
+  )
+  gh_data
+}
+
+post_data <- function() {
+  posts <- list.files(here::here("posts"), recursive = TRUE, full.names = FALSE, pattern = "^index[.]q?md")
+  post_data <- future_map_dfr(set_names(posts, dirname(posts)), function(file) {
+    data <- rmarkdown::yaml_front_matter(here::here("posts", file))
+    list(
+      date = as.Date(data$date),
+      title = data$title
+    )
+  }, .id = "dir") %>%
+    mutate(
+      type = "post",
+      link = file.path("post", dir),
+      tip = title
+    )
+  post_data
+}
+
+
+cran_pkg_data <- function(author = "qiushiyan", pkgs = c("agua")) {
+  pkg_search("Qiushi Yan") |>
+    as_tibble() |>
+    filter(package %in% pkgs) |>
+    mutate(
+      type = "pkg",
+      link = str_glue("https://CRAN.R-project.org/package={package}"),
+      tip = str_glue("{package}: {version}"),
+      date = as.Date(date),
+    )
+}
+
+fetch_repo <- function(repo) {
+  url <- str_glue("https://api.github.com/repos/qiushiyan/{repo}")
+  data <- GET(url, authenticate(Sys.getenv("GITHUB_CALENDAR_USER"), Sys.getenv("GITHUB_CALENDAR_PASSWORD"))) |> content()
+  tibble(
+    type = "project",
+    link = url,
+    tip = data$description,
+    date = as.Date(data$created_at),
+  )
+}
+
+
+repos_data <- function(repos = c(
+                         "xkcd", "uuid", "potg", "qlang", "overwatcher",
+                         "js-notebook", "nyclodging", "titanic-survival",
+                         "clipstash", "real2d", "xetra", "pyspark-cdc", "quartolive", "ymisc"
+                       )) {
+  furrr::future_map_dfr(repos, fetch_repo)
+}
+
+
+calendar_data <- function(from = NULL, to = from + years(1), github_user = "qiushiyan", repos = c(
+                            "xkcd", "uuid", "potg", "qlang", "overwatcher",
+                            "js-notebook", "nyclodging", "titanic-survival",
+                            "clipstash", "real2d", "xetra", "pyspark-cdc", "quartolive", "ymisc"
+                          )) {
+  github_data <- github_data(from, github_user)
+  if (is.null(from)) {
+    from <- today() - years(1)
+  } else {
+    from <- lubridate::as_date(from)
+  }
+
+  to <- lubridate::as_date(to)
+
+  all_data <- bind_rows(
+    select(filter(github_data, count > 0), date, type, tip, link, count),
+    select(cran_pkg_data(github_user), date, type, tip, link),
+    select(post_data(), date, type, tip, link),
+    select(repos_data(repos), date, type, tip, link)
+  )
+
+  empty_days <- filter(github_data, count == 0, !(date %in% all_data$date)) %>%
+    mutate(type = NA) %>%
+    select(date, type)
+
+  bind_rows(
+    all_data,
+    empty_days
+  ) %>% filter(date >= from, date <= to)
+}
+
+calendar_plot <- function(data = calendar_data()) {
+  p_data <- mutate(data,
+    x = floor_date(date, "week"),
+    y = fct_rev(lubridate::wday(date, label = TRUE)),
+    type = factor(type, levels = c("github", "pkg", "project", "post")),
+    on_click = str_glue('window.location.assign("{link}")'),
+    data_id = ifelse(!is.na(type), as.character(seq_along(x)), NA_character_),
+    tip = str_glue("<b>{tip}</b> on {date}")
+  )
+
+  counts <- count(p_data, type, .drop = FALSE) %>%
+    na.omit() %>%
+    mutate(
+      n = case_when(
+        type == "github" ~ count(p_data |> filter(!is.na(type)), type, wt = as.integer(count)) %>% pull(),
+        TRUE ~ n
+      ),
+      emoji = c("💻", "📋", "💡", "📦"),
+      nice = c("GitHub", "posts", "projects", "CRAN"),
+      label = str_glue("{scales::comma_format(accuracy = 1)(n)} {emoji}\n{nice}")
+    )
+
+  my_pal <- solarized_pal("blue")(4)
+  rebase <- ggthemes:::solarized_rebase()
+
+  p <- ggplot() +
+    geom_tile(data = filter(p_data, is.na(type)), width = 6, height = .8, aes(x, y, fill = type), key_glyph = draw_key_point) +
+    geom_tile_interactive(
+      aes(x, y, fill = type, alpha = log(as.numeric(count)),
+        tooltip = tip, onclick = on_click, data_id = data_id),
+      data = filter(p_data, !is.na(type)),
+      width = 6, height = .8) +
+    scale_fill_manual(
+      values = c("github" = my_pal[[1]], "post" = my_pal[[2]], "pkg" = my_pal[[3]], "project" = my_pal[[4]]),
+      breaks = c("github", "post", "pkg", "project"),
+      labels = counts$label,
+      na.translate = TRUE, na.value = "#eee8d5"
+    ) +
+    scale_y_discrete(breaks = c("Mon", "Wed", "Fri")) +
+    theme_solarized() +
+    theme(
+      rect = element_blank(),
+      line = element_blank(),
+      plot.background = element_rect(fill = "#0f182b", color = NA),
+      text = element_text(family = "Atkinson Hyperlegible"),
+      axis.text = element_text(color = "white", size = 9),
+      legend.position = "bottom",
+      aspect.ratio = .8 / 6
+    ) +
+    guides(
+      alpha = "none",
+      fill = guide_legend(
+        title = NULL,
+        override.aes = list(shape = "square filled", size = 3),
+        keywidth = 3,
+        label.theme = element_text(family = "Atkinson Hyperlegible", size = 9),
+        label.position = "bottom",
+        nrow = 1
+      )
+    ) +
+    labs(x = NULL, y = NULL)
+
+  x <- girafe(code = print(p), width_svg = 6, height_svg = 2)
+
+  tooltip_css <- "background: #002b36; opacity: 1; color: #839496; border-radius: 5px;
+  padding: 5px; box-shadow: 3px 3px 5px 0px #888888;
+  font-size: 16px; border-width 2px; border-color: #002b36;"
+
+  girafe_options(x, opts_tooltip(css = tooltip_css))
+}
+
+if (fs::file_exists("calendar_data.rds")) {
+  calendar_data <- calendar_data()
+  write_rds(calendar_data, "calendar_data.rds")
+} else {
+  calendar_data <- read_rds("calendar_data.rds")
+}
